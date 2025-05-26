a7ff71f2a151d3577332c3420ccf9bb4995c03f7
robertoDorado <robertodorado7@gmail.com>
2025-05-26 15:36:47 -0300

    arquivo shell de migration atualizada

--- shell/migrations.sh
+++ shell/migrations.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-PHP_FILES=($(ls -r source/Migrations/*.php))
-for FILE in "${PHP_FILES[@]}"; do
-    php "$FILE"
-done
+
+MIGRATIONS_DIR="source/Migrations"
+
+if [ $# -eq 1 ]; then
+    TARGET_FILE="$MIGRATIONS_DIR/$1.php"
+    if [ -f "$TARGET_FILE" ]; then
+        php "$TARGET_FILE"
+    else
+        echo "Arquivo '$TARGET_FILE' não encontrado."
+        exit 1
+    fi
+else
+    PHP_FILES=($(ls -r "$MIGRATIONS_DIR"/*.php))
+    for FILE in "${PHP_FILES[@]}"; do
+        php "$FILE"
+    done
+fi
